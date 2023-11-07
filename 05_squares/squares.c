25cf1fefebf38a91b1c632e2bbd3cf937af5dd19
Coursera Learner <nobody@nowhere.nul>
2023-11-07 14:47:18 +0000

    complete

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -9,8 +9,13 @@
 int isInRange(int coord, int offset, int size) {
   // if coord is in range, return 1
   // else, return 0
-  return 0;
+  if (coord >= offset && coord < offset + size){
+	  return 1;
+  } else {
+	  return 0;
+  }
 }
+
 
 /*
  * Determines if coord is at border of offset or
@@ -19,18 +24,20 @@ int isInRange(int coord, int offset, int size) {
 int isAtBorder(int coord, int offset, int size) {
   // if coord is equal to offest or offset + size
   // return 1, else return 0
-  return 0;
+  if (coord == offset || coord == offset + size - 1) {
+	  return 1;
+  } else {
+	  return 0;
+  }
 }
 
 void squares(int size1, int x_offset, int y_offset, int size2) {
   //compute the max of size1 and (x_offset + size2).  Call this w
-
+	int w = (size1 > x_offset + size2) ? size1 : x_offset + size2;
   //compute the max of size1 and (y_offset + size2).  Call this h
-
+	int h = (size1 > y_offset + size2) ? size1 : y_offset + size2;
   //count from 0 to h. Call the number you count with y
-
     //count from 0 to w. Call the number you count with x
-
       //check if  EITHER
       //    ((x is between x_offset  and x_offset +size2) AND 
       //     y is equal to either y_offset OR y_offset + size2 - 1 )
@@ -49,5 +56,19 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       //else print a space
     //when you finish counting x from 0 to w, 
     //print a newline
+    for (int y = 0; y < h; y++) {
+	    for (int x = 0; x < w; x++) {
+		    if ((isInRange(x, x_offset, size2) && isAtBorder(y, y_offset, size2)) || (isInRange(y, y_offset, size2) && isAtBorder(x, x_offset, size2))) {
+			    printf("*");
+		    }
+		    else if ((x < size1 && (y == 0 || y == size1 - 1)) || (y < size1 && (x == 0 || x == size1 - 1))) {
+			    printf("#");
+		    }
+		    else {
+			    printf(" ");
+		    }
+	    }
+	    printf("\n");
+    }
 
 }
